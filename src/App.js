7e152064e9543aef3a25916234f6f7ec51c79342
sakagawayasuaki <stream.yasuaki@gmail.com>
2023-09-30 21:53:45 +0900

    secret key指定

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ export const Pathcontext = createContext();
 export const Videocontext = createContext();
 export const Transcriptcontext = createContext();
 export const TxtContext = createContext();
-export const root = createContext("http://127.0.0.1:8000/"); //https://giziroku-fastapi-docker.azurewebsites.net
+export const root = createContext("https://giziroku-fastapi-docker.azurewebsites.net"); //https://giziroku-fastapi-docker.azurewebsites.net
 export const Precisioncontext = createContext();
 
 function App() {
