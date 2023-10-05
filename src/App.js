536b672426cbc6afcb7f3f50ddf176d646dc15cf
sakagawayasuaki <stream.yasuaki@gmail.com>
2023-10-05 19:38:07 +0900

    最新のリモートをマージ

--- src/App.js
+++ src/App.js
@@ -38,7 +38,6 @@ export const Pathcontext = createContext();
 export const Videocontext = createContext();
 export const Transcriptcontext = createContext();
 export const TxtContext = createContext();
-
 export const root = createContext(
   "https://giziroku-fastapi-docker.azurewebsites.net/"
 ); //http://127.0.0.1:8000///https://giziroku-fastapi-docker.azurewebsites.net
